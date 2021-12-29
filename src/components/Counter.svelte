7ef2cff3193522997ed18d2925054d1711fead10
nabameet <thenabameet@gmail.com>
2021-12-29 18:45:53 +0530

    first commit

---
+++ src/components/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+	let count = 0;
+	const increment = () => count++;
+</script>
+
+<button
+	class="mx-10 py-1 hehe text-2xl bg-gray-900 shadow-sm text-white px-6 rounded"
+	on:click={increment}
+>
+	Clicks: {count}
+</button>
